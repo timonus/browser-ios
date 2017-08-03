7d862b78f1f9b4dd9c8087affebe53123c93f700
Joel Reis <joelhreis@gmail.com>
2017-08-03 10:50:12 -0400

    Pull Swedish language from Transifex, so strings are actually translated.

--- brave/l10n/downloadLanguages.js
+++ brave/l10n/downloadLanguages.js
@@ -20,7 +20,7 @@ const fs = require('fs')
 const request = require('request')
 
 // The names of the directories in the locales folder are used as a list of languages to retrieve
-var languages = [ "fr", "pl", "ru", "de", "zh", "zh_TW", "id_ID", "it", "ja", "ko_KR", "ms", "pt_BR", "es", "uk", "nb" ]
+var languages = [ "fr", "pl", "ru", "de", "zh", "zh_TW", "id_ID", "it", "ja", "ko_KR", "ms", "pt_BR", "es", "uk", "nb", "sv" ]
 // Support retrieving a single language
 if (process.env.LANG_CODE) {
   languages = [process.env.LANG_CODE]
