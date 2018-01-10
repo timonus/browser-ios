658f4d46df440fc36e27ebedd0a431045a4fe104
Michal Buczek <michal.buczek@protonmail.com>
2018-01-10 20:58:54 +0100

    Refactor: extract thumbnail image setup to separate struct.

---
+++ brave/src/frontend/favourites/FavoritesTileDecorator.swift
@@ -0,0 +1,86 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import UIKit
+import Storage
+
+enum FavoritesTileType {
+    /// Predefinied tile color and custom icon, used for most popular websites.
+    case preset
+    /// Just a favicon, no background color.
+    case faviconOnly
+    /// A globe icon, no background color.
+    case defaultTile
+}
+
+struct FavoritesTileDecorator {
+    let url: URL
+    let normalizedHost: String
+    let cell: ThumbnailCell
+
+    /// Returns SuggestedSite for given tile or nil if no suggested sites found.
+    var commonWebsite: SuggestedSite? {
+        let suggestedSites = SuggestedSites.asArray()
+
+        return suggestedSites.filter { site in
+            extractDomainURL(site.url) == normalizedHost
+            }.first
+    }
+
+    var tileType: FavoritesTileType {
+        if commonWebsite != nil {
+            return .preset
+        } else if ImageCache.shared.hasImage(url, type: .square) {
+            return .faviconOnly
+        } else {
+            return .defaultTile
+        }
+    }
+
+    init(url: URL, cell: ThumbnailCell) {
+        self.url = url
+        self.cell = cell
+        normalizedHost = url.normalizedHost ?? ""
+    }
+
+    func decorateTile() {
+        switch tileType {
+        case .preset:
+            // FIXME: Split it into separate guard clauses to give more specific error logs in case something is missing?
+            guard let website = commonWebsite, let iconUrl = website.wordmark.url.asURL, let host = iconUrl.host,
+                iconUrl.scheme == "asset", let image = UIImage(named: host) else {
+                    setDefaultTile()
+                    return
+            }
+
+            cell.imageView.backgroundColor = website.backgroundColor
+            cell.imageView.contentMode = .scaleAspectFit
+            cell.imageView.layer.minificationFilter = kCAFilterTrilinear
+            cell.showBorder(!PrivateBrowsing.singleton.isOn)
+
+            UIGraphicsBeginImageContextWithOptions(image.size, false, 0)
+            image.draw(in: CGRect(origin: CGPoint(x: 3, y: 6), size: CGSize(width: image.size.width - 6, height: image.size.height - 6)))
+            let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            cell.imageView.image = scaledImage
+            break
+        case .faviconOnly:
+            ImageCache.shared.image(url, type: .square, callback: { (image) in
+                postAsyncToMain {
+                    self.cell.imageView.image = image
+                }
+            })
+            break
+        case .defaultTile:
+            setDefaultTile()
+            break
+        }
+    }
+
+    private func setDefaultTile() {
+        cell.imageView.image = FaviconFetcher.defaultFavicon
+    }
+
+    private func extractDomainURL(_ url: String) -> String {
+        return URL(string: url)?.normalizedHost ?? url
+    }
+}
