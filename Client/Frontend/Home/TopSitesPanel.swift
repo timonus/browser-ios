b34d4f6f0c3c87a59d9e82716a4348016fb16898
Michal Buczek <michal.buczek@protonmail.com>
2018-01-11 13:54:37 +0100

    Further TopSitesPanel refactor, made views lazy, more cleanup.

--- Client/Frontend/Home/TopSitesPanel.swift
+++ Client/Frontend/Home/TopSitesPanel.swift
@@ -20,15 +20,9 @@ struct TopSitesPanelUX {
 
 class TopSitesPanel: UIViewController, HomePanel {
     weak var homePanelDelegate: HomePanelDelegate?
-    fileprivate var collection: UICollectionView? = nil
-    fileprivate var privateTabMessageContainer: UIView!
-    fileprivate var privateTabGraphic: UIImageView!
-    fileprivate var privateTabTitleLabel: UILabel!
-    fileprivate var privateTabInfoLabel: UILabel!
-    fileprivate var privateTabLinkButton: UIButton!
-    fileprivate var braveShieldStatsView: BraveShieldStatsView? = nil
-    fileprivate lazy var dataSource: FavoritesDataSource = { return FavoritesDataSource() }()
-    fileprivate lazy var layout: UICollectionViewFlowLayout = {
+
+    // MARK: - Favorites collection view properties
+    fileprivate lazy var collection: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
 
         // TODO: A bit larger thumbnails for iPhone horizontal? Currently it shows 7 sites.
@@ -37,18 +31,78 @@ class TopSitesPanel: UIViewController, HomePanel {
         layout.minimumInteritemSpacing = 4
         layout.minimumLineSpacing = 4
 
-        return layout
+        let view = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        view.backgroundColor = PrivateBrowsing.singleton.isOn ? BraveUX.BackgroundColorForTopSitesPrivate : BraveUX.BackgroundColorForBookmarksHistoryAndTopSites
+        view.delegate = self
+
+        let thumbnailIdentifier = "Thumbnail"
+        view.register(ThumbnailCell.self, forCellWithReuseIdentifier: thumbnailIdentifier)
+        view.keyboardDismissMode = .onDrag
+        view.accessibilityIdentifier = "Top Sites View"
+        // Entire site panel, including the stats view insets
+        view.contentInset = UIEdgeInsetsMake(TopSitesPanelUX.statsHeight, 0, 0, 0)
+
+        return view
+    }()
+    fileprivate lazy var dataSource: FavoritesDataSource = { return FavoritesDataSource() }()
+
+    // MARK: - Lazy views
+    fileprivate lazy var privateTabMessageContainer: UIView = {
+        let view = UIView()
+        view.isUserInteractionEnabled = true
+        view.isHidden = !PrivateBrowsing.singleton.isOn
+        return view
     }()
 
-    private let ThumbnailIdentifier = "Thumbnail"
+    fileprivate lazy var privateTabGraphic: UIImageView = {
+        return UIImageView(image: UIImage(named: "privateLion"))
+    }()
 
-    override var supportedInterfaceOrientations : UIInterfaceOrientationMask {
-        return UIInterfaceOrientationMask.allButUpsideDown
-    }
+    fileprivate lazy var privateTabTitleLabel: UILabel = {
+        let view = UILabel()
+        view.lineBreakMode = .byWordWrapping
+        view.textAlignment = .center
+        view.numberOfLines = 0
+        view.font = UIFont.systemFont(ofSize: 18, weight: UIFontWeightSemibold)
+        view.textColor = UIColor(white: 1, alpha: 0.6)
+        view.text = Strings.Private_Tab_Title
+        return view
+    }()
 
+    fileprivate lazy var privateTabInfoLabel: UILabel = {
+        let view = UILabel()
+        view.lineBreakMode = .byWordWrapping
+        view.textAlignment = .center
+        view.numberOfLines = 0
+        view.font = UIFont.systemFont(ofSize: 14, weight: UIFontWeightMedium)
+        view.textColor = UIColor(white: 1, alpha: 1.0)
+        view.text = Strings.Private_Tab_Body
+        return view
+    }()
+
+    fileprivate lazy var privateTabLinkButton: UIButton = {
+        let view = UIButton()
+        let linkButtonTitle = NSAttributedString(string: Strings.Private_Tab_Link, attributes:
+            [NSUnderlineStyleAttributeName: NSUnderlineStyle.styleSingle.rawValue])
+        view.setAttributedTitle(linkButtonTitle, for: .normal)
+        view.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: UIFontWeightMedium)
+        view.titleLabel?.textColor = UIColor(white: 1, alpha: 0.25)
+        view.titleLabel?.textAlignment = .center
+        view.titleLabel?.lineBreakMode = .byWordWrapping
+        view.addTarget(self, action: #selector(showPrivateTabInfo), for: .touchUpInside)
+        return view
+    }()
+
+    fileprivate lazy var braveShieldStatsView: BraveShieldStatsView = {
+        let view = BraveShieldStatsView(frame: CGRect.zero)
+        view.autoresizingMask = [.flexibleWidth]
+        return view
+    }()
+
+    // MARK: - Init/lifecycle
     init() {
         super.init(nibName: nil, bundle: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(TopSitesPanel.privacyModeChanged), name: NotificationPrivacyModeChanged, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(TopSitesPanel.privateBrowsingModeChanged), name: NotificationPrivacyModeChanged, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(TopSitesPanel.updateIphoneConstraints), name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
     }
 
@@ -56,73 +110,20 @@ class TopSitesPanel: UIViewController, HomePanel {
         fatalError("init(coder:) has not been implemented")
     }
 
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: NotificationPrivacyModeChanged, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = PrivateBrowsing.singleton.isOn ? BraveUX.BackgroundColorForTopSitesPrivate : BraveUX.BackgroundColorForBookmarksHistoryAndTopSites
-        
-        privateTabMessageContainer = UIView()
-        privateTabMessageContainer.isUserInteractionEnabled = true
-        privateTabMessageContainer.isHidden = !PrivateBrowsing.singleton.isOn
-        
-        privateTabGraphic = UIImageView(image: UIImage(named: "privateLion"))
-        privateTabMessageContainer.addSubview(privateTabGraphic)
-        
-        privateTabTitleLabel = UILabel()
-        privateTabTitleLabel.lineBreakMode = .byWordWrapping
-        privateTabTitleLabel.textAlignment = .center
-        privateTabTitleLabel.numberOfLines = 0
-        privateTabTitleLabel.font = UIFont.systemFont(ofSize: 18, weight: UIFontWeightSemibold)
-        privateTabTitleLabel.textColor = UIColor(white: 1, alpha: 0.6)
-        privateTabTitleLabel.text = Strings.Private_Tab_Title
-        privateTabMessageContainer.addSubview(privateTabTitleLabel)
-        
-        privateTabInfoLabel = UILabel()
-        privateTabInfoLabel.lineBreakMode = .byWordWrapping
-        privateTabInfoLabel.textAlignment = .center
-        privateTabInfoLabel.numberOfLines = 0
-        privateTabInfoLabel.font = UIFont.systemFont(ofSize: 14, weight: UIFontWeightMedium)
-        privateTabInfoLabel.textColor = UIColor(white: 1, alpha: 1.0)
-        privateTabInfoLabel.text = Strings.Private_Tab_Body
-        privateTabMessageContainer.addSubview(privateTabInfoLabel)
-        
-        privateTabLinkButton = UIButton()
-        let linkButtonTitle = NSAttributedString(string: Strings.Private_Tab_Link, attributes:
-            [NSUnderlineStyleAttributeName: NSUnderlineStyle.styleSingle.rawValue])
-        privateTabLinkButton.setAttributedTitle(linkButtonTitle, for: .normal)
-        privateTabLinkButton.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: UIFontWeightMedium)
-        privateTabLinkButton.titleLabel?.textColor = UIColor(white: 1, alpha: 0.25)
-        privateTabLinkButton.titleLabel?.textAlignment = .center
-        privateTabLinkButton.titleLabel?.lineBreakMode = .byWordWrapping
-        privateTabLinkButton.addTarget(self, action: #selector(showPrivateTabInfo), for: .touchUpInside)
-        privateTabMessageContainer.addSubview(privateTabLinkButton)
 
-        let collection = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
-        collection.backgroundColor = PrivateBrowsing.singleton.isOn ? BraveUX.BackgroundColorForTopSitesPrivate : BraveUX.BackgroundColorForBookmarksHistoryAndTopSites
-        collection.delegate = self
-        collection.dataSource = PrivateBrowsing.singleton.isOn ? nil : dataSource
-        collection.register(ThumbnailCell.self, forCellWithReuseIdentifier: ThumbnailIdentifier)
-        collection.keyboardDismissMode = .onDrag
-        collection.accessibilityIdentifier = "Top Sites View"
-        // Entire site panel, including the stats view insets
-        collection.contentInset = UIEdgeInsetsMake(TopSitesPanelUX.statsHeight, 0, 0, 0)
         view.addSubview(collection)
-        collection.snp.makeConstraints { make -> Void in
-            if #available(iOS 11.0, *) {
-                make.edges.equalTo(self.view.safeAreaLayoutGuide.snp.edges)
-            } else {
-                make.edges.equalTo(self.view)
-            }
-        }
+        collection.dataSource = PrivateBrowsing.singleton.isOn ? nil : dataSource
+        self.dataSource.collectionView = self.collection
 
-        self.collection = collection
-        
-        let braveShieldStatsView = BraveShieldStatsView(frame: CGRect.zero)
-        collection.addSubview(braveShieldStatsView)
-        self.braveShieldStatsView = braveShieldStatsView
-        
-        collection.addSubview(privateTabMessageContainer)
-        
         // Could setup as section header but would need to use flow layout,
         // Auto-layout subview within collection doesn't work properly,
         // Quick-and-dirty layout here.
@@ -133,10 +134,28 @@ class TopSitesPanel: UIViewController, HomePanel {
         statsViewFrame.size.width = collection.frame.width - statsViewFrame.minX * 2
         statsViewFrame.size.height = TopSitesPanelUX.statsHeight
         braveShieldStatsView.frame = statsViewFrame
-        braveShieldStatsView.autoresizingMask = [.flexibleWidth]
 
-        self.dataSource.collectionView = self.collection
-        
+        collection.addSubview(braveShieldStatsView)
+
+        privateTabMessageContainer.addSubview(privateTabGraphic)
+        privateTabMessageContainer.addSubview(privateTabTitleLabel)
+        privateTabMessageContainer.addSubview(privateTabInfoLabel)
+        privateTabMessageContainer.addSubview(privateTabLinkButton)
+        collection.addSubview(privateTabMessageContainer)
+
+        makeConstraints()
+    }
+
+    // MARK: - Constraints setup
+    fileprivate func makeConstraints() {
+        collection.snp.makeConstraints { make -> Void in
+            if #available(iOS 11.0, *) {
+                make.edges.equalTo(self.view.safeAreaLayoutGuide.snp.edges)
+            } else {
+                make.edges.equalTo(self.view)
+            }
+        }
+
         privateTabMessageContainer.snp.makeConstraints { (make) -> Void in
             make.centerX.equalTo(collection)
             if UIDevice.current.userInterfaceIdiom == .pad {
@@ -144,35 +163,35 @@ class TopSitesPanel: UIViewController, HomePanel {
                 make.width.equalTo(400)
             }
             else {
-                make.top.equalTo(self.braveShieldStatsView?.snp.bottom ?? 0).offset(20)
+                make.top.equalTo(self.braveShieldStatsView.snp.bottom).offset(20)
                 make.leftMargin.equalTo(collection).offset(8)
                 make.rightMargin.equalTo(collection).offset(-8)
             }
             make.bottom.equalTo(collection)
         }
-        
+
         privateTabGraphic.snp.makeConstraints { (make) -> Void in
             make.top.equalTo(0)
             make.centerX.equalTo(self.privateTabMessageContainer)
         }
-        
+
         if UIDevice.current.userInterfaceIdiom == .pad {
             privateTabTitleLabel.snp.makeConstraints { make in
                 make.top.equalTo(self.privateTabGraphic.snp.bottom).offset(15)
                 make.centerX.equalTo(self.privateTabMessageContainer)
                 make.left.right.equalTo(0)
             }
-            
+
             privateTabInfoLabel.snp.makeConstraints { (make) -> Void in
                 make.top.equalTo(self.privateTabTitleLabel.snp.bottom).offset(10)
                 if UIDevice.current.userInterfaceIdiom == .pad {
                     make.centerX.equalTo(collection)
                 }
-                
+
                 make.left.equalTo(16)
                 make.right.equalTo(-16)
             }
-            
+
             privateTabLinkButton.snp.makeConstraints { (make) -> Void in
                 make.top.equalTo(self.privateTabInfoLabel.snp.bottom).offset(10)
                 make.left.equalTo(0)
@@ -188,7 +207,7 @@ class TopSitesPanel: UIViewController, HomePanel {
         // Not sure why but when a side panel is opened and you transition from portait to landscape
         // top site cells are misaligned, this is a workaroud for this edge case. Happens only on iPhoneX.
         if #available(iOS 11.0, *), DeviceDetector.iPhoneX {
-            collection?.snp.remakeConstraints { make -> Void in
+            collection.snp.remakeConstraints { make -> Void in
                 make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
                 make.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom)
                 make.leading.equalTo(self.view.safeAreaLayoutGuide.snp.leading)
@@ -235,27 +254,24 @@ class TopSitesPanel: UIViewController, HomePanel {
         
         self.view.setNeedsUpdateConstraints()
     }
-    
-    deinit {
-        NotificationCenter.default.removeObserver(self, name: NotificationPrivacyModeChanged, object: nil)
-        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
-    }
 
-    func privacyModeChanged() {
+
+    // MARK: - Private browsing modde
+    func privateBrowsingModeChanged() {
         let isPrivateBrowsing = PrivateBrowsing.singleton.isOn
 
         // TODO: This entire blockshould be abstracted
         //  to make code in this class DRY (duplicates from elsewhere)
-        collection?.backgroundColor = isPrivateBrowsing ? BraveUX.BackgroundColorForTopSitesPrivate : BraveUX.BackgroundColorForBookmarksHistoryAndTopSites
+        collection.backgroundColor = isPrivateBrowsing ? BraveUX.BackgroundColorForTopSitesPrivate : BraveUX.BackgroundColorForBookmarksHistoryAndTopSites
         privateTabMessageContainer.isHidden = !isPrivateBrowsing
-        braveShieldStatsView?.timeStatView.color = isPrivateBrowsing ? .white : .black
+        braveShieldStatsView.timeStatView.color = isPrivateBrowsing ? .white : .black
         // Handling edge case when app starts in private only browsing mode and is switched back to normal mode.
-        if collection?.dataSource == nil && !isPrivateBrowsing {
-            collection?.dataSource = dataSource
+        if collection.dataSource == nil && !isPrivateBrowsing {
+            collection.dataSource = dataSource
         } else if isPrivateBrowsing {
-            collection?.dataSource = nil
+            collection.dataSource = nil
         }
-        collection?.reloadData()
+        collection.reloadData()
     }
     
     func showPrivateTabInfo() {
@@ -265,33 +281,14 @@ class TopSitesPanel: UIViewController, HomePanel {
             _ = t?.addTabAndSelect(URLRequest(url: url))
         }
     }
-
-    fileprivate func deleteOrUpdateSites(_ indexPath: IndexPath) -> Success {
-        guard let collection = self.collection else { return succeed() }
-
-        let result = Success()
-
-        collection.performBatchUpdates({
-            collection.deleteItems(at: [indexPath as IndexPath])
-
-            // If we have more items in our data source, replace the deleted site with a new one.
-            let count = collection.numberOfItems(inSection: 0) - 1
-            if let frcCount = self.dataSource.frc?.fetchedObjects?.count, count < frcCount {
-                collection.insertItems(at: [ IndexPath(item: count, section: 0) ])
-            }
-        }, completion: { _ in
-            result.fill(Maybe(success: ()))
-        })
-
-        return result
-    }
 }
 
+// MARK: - Delegates
 extension TopSitesPanel: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let fav = dataSource.frc?.object(at: indexPath) as! Bookmark
+        let fav = dataSource.frc?.object(at: indexPath) as? Bookmark
 
-        guard let urlString = fav.url, let url = URL(string: urlString) else { return }
+        guard let urlString = fav?.url, let url = URL(string: urlString) else { return }
 
         homePanelDelegate?.homePanel(self, didSelectURL: url)
     }
